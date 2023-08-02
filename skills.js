7064b8b1caa3fb879cbd94efb321af4fc0b78967
manisharma31 <40764742+manisharma31@users.noreply.github.com>
2023-08-02 11:36:07 +0000

    Copilot first commit

--- skills.js
+++ skills.js
@@ -1,3 +1,7 @@
-function calculatenumbers(var1,var2){
-    return var1+var2;
+function calculateSkillPoints() {
+    var skillPoints = 0;
+    for (var i = 0; i < 10; i++) {
+        skillPoints += parseInt(document.getElementById("skill" + i).value);
+    }
+    document.getElementById("skillPoints").innerHTML = skillPoints;
 }
